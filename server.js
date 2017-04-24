6711eca80d9a0778cc768d8b0c946992c86641cd
Jack Burns <Jack Burns>
2017-04-23 21:35:13 -0700

    Add start script to package.json

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
 // Initialize the app.
