813a243abc842a54abe2038bb085d7b31426f383
Jack Burns <Jack Burns>
2017-04-23 21:26:29 -0700

    Make available on all ports

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(bodyParser.json());
 
 // Initialize the app.
-var server = app.listen(/*process.env.PORT ||*/ 8080, function () {
+var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
 });
