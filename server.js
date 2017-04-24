f3be5f70accee93fae4f6a1efa54c127d30458d0
Jack Burns <Jack Burns>
2017-04-23 21:25:10 -0700

    Fix server.js

--- server.js
+++ server.js
@@ -1,35 +1,15 @@
-var express = require("express");
-var path = require("path");
-var bodyParser = require("body-parser");
-var mongodb = require("mongodb");
-var ObjectID = mongodb.ObjectID;
+var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
-app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
-
-// Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-  // Save database object from the callback for reuse.
-  db = database;
-  console.log("Database connection ready");
-
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
+// Initialize the app.
+var server = app.listen(/*process.env.PORT ||*/ 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
-  });
 });
 
-// CONTACTS API ROUTES BELOW
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
@@ -37,11 +17,8 @@ function handleError(res, reason, message, code) {
   res.status(code || 500).json({"error": message});
 }
 
-/*  "/contacts"
- *    GET: finds all contacts
- *    POST: creates a new contact
- */
 
-app.get("/", function(req, res) {
+//GET
+app.get('/', function(req, res) {
   res.send("Request Handled");
 });
